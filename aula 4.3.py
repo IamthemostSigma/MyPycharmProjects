94fe8b35f60b646e76793f0906d3f17bbaaaab8a
IamthemostSigma <Salvador9633@icloud.com>
2024-10-26 12:53:50 +0100

    Add files via upload

---
+++ aula 4.3.py
@@ -0,0 +1,9 @@
+x = int(input("Quanto mede o lado do quadrado?: "))
+
+Área= x**2 #quando queremos fazer uma potencia em python, usamos este simbolo [**]
+#ao quadrado = **2
+#ao cubo = **3
+#etc
+perimetro = x*4
+
+print(f"A medida do perímetro do quadrado é: {perimetro}, e a medida da área é: {Área}")
