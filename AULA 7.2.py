4a41524b3f8aa46615803d816920fc90cd05800c
IamthemostSigma <Salvador9633@icloud.com>
2024-11-16 12:50:42 +0000

    Add files via upload

---
+++ AULA 7.2.py
@@ -0,0 +1,9 @@
+nota1 = int(input("Diga uma nota sua "))
+nota2 = int(input("Diga outra nota sua"))
+
+calculo = (nota1 + nota2) /2
+
+if calculo >=60 :
+    print("Você está aprovado!(;")
+else:
+    print("você está reprovado.);")
